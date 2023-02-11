31c35ea80a86a6390e37cd0291137dd9e6653086
Zu1779 <zu1779@gmail.com>
2023-02-11 16:15:53 +0100

    custom TrackForEachProgress

--- Zu1779.EtaCalculator/Zu1779.EtaCalculator/EtaCalculator.cs
+++ Zu1779.EtaCalculator/Zu1779.EtaCalculator/EtaCalculator.cs
@@ -1,164 +1,160 @@
-﻿namespace Zu1779.EtaCalculator
+﻿using System;
+using System.Timers;
+
+using Zu1779.GenUtil.Extension.ComparableExtension;
+
+namespace Zu1779.EtaCalculator;
+
+//TODO: integrate classic for cycle for data acquisition?
+//TODO: integrate a conditional dump of the state based on count or time gap
+
+//TODO: implement an option to never approximate to (done 100% or todo 0%) if not really finished
+//TODO: implement the possibility to choose the data span from which calculate statistics (all, last minute, last timespan)
+
+//TODO: check optional nuget to use as plugin
+//TODO: refactoring with EtaCalculator state (stopped, ...)
+//TODO: decide what to do in a unknown total state, and refactoring accordingly
+
+public class EtaCalculator : IEtaCalculator
 {
-    using System;
-    using System.Collections;
-    using System.Collections.Generic;
-    using System.Diagnostics;
-    using System.Timers;
-
-    using Zu1779.GenUtil;
-
-    //TODO: integrate classic for cycle for data acquisition?
-    //TODO: integrate a conditional dump of the state based on count or time gap
-
-    //TODO: implement an option to never approximate to (done 100% or todo 0%) if not really finished
-    //TODO: implement the possibility to choose the data span from which calculate statistics (all, last minute, last timespan)
-
-    //TODO: check optional nuget to use as plugin
-    //TODO: refactoring with EtaCalculator state (stopped, ...)
-    //TODO: decide what to do in a unknown total state, and refactoring accordingly
-
-    public class EtaCalculator : IEtaCalculator
+    public EtaCalculator(IStopwatch stopwatch = null)
     {
-        public EtaCalculator(IStopwatch stopwatch = null)
-        {
-            this.stopwatch = stopwatch ?? new StopwatchWrapper();
-            thresholdTimer.Elapsed += (s, e) => OnTimeProgress(Done, Total.Value);
-        }
+        this.stopwatch = stopwatch ?? new StopwatchWrapper();
+        thresholdTimer.Elapsed += (s, e) => OnTimeProgress(Done, Total.Value);
+    }
 
-        private readonly IStopwatch stopwatch;
-        private readonly Timer thresholdTimer = new Timer { AutoReset = true, Enabled = false, };
-        //private double? lastCountProgress;
-        private void StartThresholdTimer()
+    private readonly IStopwatch stopwatch;
+    private readonly Timer thresholdTimer = new Timer { AutoReset = true, Enabled = false, };
+    //private double? lastCountProgress;
+    private void StartThresholdTimer()
+    {
+        if (!thresholdTimer.Enabled && TimeThreshold.HasValue)
         {
-            if (!thresholdTimer.Enabled && TimeThreshold.HasValue)
+            thresholdTimer.Interval = TimeThreshold.Value.TotalMilliseconds;
+            thresholdTimer.Start();
+        }
+    }
+    private void StopThresholdTimer()
+    {
+        if (thresholdTimer.Enabled) thresholdTimer.Stop();
+    }
+
+    public event EventHandler<EtaCountEventArgs> CountProgress;
+    public event EventHandler<EtaTimeEventArgs> TimeProgress;
+    public event EventHandler<EtaEventArgs> Progress;
+    private void OnCountProgress(double done, double? total)
+    {
+        var e = new EtaCountEventArgs { Done = done, Total = total };
+        CountProgress?.Invoke(this, e);
+        OnProgress(e);
+    }
+    private void OnTimeProgress(double done, double? total)
+    {
+        var e = new EtaTimeEventArgs { Done = done, Total = total };
+        TimeProgress?.Invoke(this, e);
+        OnProgress(e);
+    }
+    private void OnProgress(EtaEventArgs e) => Progress?.Invoke(this, e);
+
+    public DateTimeOffset? StartTime { get; private set; }
+    public DateTimeOffset? StopTime
+    {
+        get
+        {
+            if (!StartTime.HasValue || !TotalTime.HasValue) return null;
+            else return StartTime.Value.Add(TotalTime.Value);
+        }
+    }
+
+    public double? Total { get; set; }
+    private double done;
+    public double Done
+    {
+        get => done;
+        set
+        {
+            done = value;
+            if (done == Total)
             {
-                thresholdTimer.Interval = TimeThreshold.Value.TotalMilliseconds;
-                thresholdTimer.Start();
+                stopwatch.Stop();
+                StopThresholdTimer();
+            }
+            // Consider threshold as percentage
+            if (CountThreshold >= 0 && CountThreshold < 1)
+            {
+                //TODO: Check if _done pass thresold
+            }
+            else if (CountThreshold >= 1)
+            {
+                //TODO: implement a raise mode to permit advance of values different of 1
+                if (done % CountThreshold == 0) OnCountProgress(Done, Total);
             }
         }
-        private void StopThresholdTimer()
-        {
-            if (thresholdTimer.Enabled) thresholdTimer.Stop();
-        }
+    }
+    public double? DoneProportion
+    {
+        get => Done / Total;
+        set => Done = (value * Total) ?? 0;
+    }
+    public double? ToDo
+    {
+        get => Total - Done;
+        set => Done = (Total - ToDo) ?? 0;
+    }
+    public double? ToDoProportion
+    {
+        get => 1 - DoneProportion;
+        set => DoneProportion = 1 - value;
+    }
 
-        public event EventHandler<EtaCountEventArgs> CountProgress;
-        public event EventHandler<EtaTimeEventArgs> TimeProgress;
-        public event EventHandler<EtaEventArgs> Progress;
-        private void OnCountProgress(double done, double? total)
-        {
-            var e = new EtaCountEventArgs { Done = done, Total = total };
-            CountProgress?.Invoke(this, e);
-            OnProgress(e);
-        }
-        private void OnTimeProgress(double done, double? total)
-        {
-            var e = new EtaTimeEventArgs { Done = done, Total = total };
-            TimeProgress?.Invoke(this, e);
-            OnProgress(e);
-        }
-        private void OnProgress(EtaEventArgs e) => Progress?.Invoke(this, e);
+    public double? CountThreshold { get; set; }
+    public TimeSpan? TimeThreshold { get; set; }
 
-        public DateTimeOffset? StartTime { get; private set; }
-        public DateTimeOffset? StopTime
+    public TimeSpan? TotalTime
+    {
+        get
         {
-            get
-            {
-                if (!StartTime.HasValue || !TotalTime.HasValue) return null;
-                else return StartTime.Value.Add(TotalTime.Value);
-            }
+            if (!Total.HasValue || Done == 0) return null;
+            else return TimeSpan.FromMilliseconds(stopwatch.Elapsed.TotalMilliseconds / Done * Total.Value);
         }
+    }
+    public TimeSpan DoneTime => stopwatch.Elapsed;
+    public TimeSpan? ToDoTime => TotalTime?.Subtract(DoneTime);
 
-        public double? Total { get; set; }
-        private double done;
-        public double Done
-        {
-            get => done;
-            set
-            {
-                done = value;
-                if (done == Total)
-                {
-                    stopwatch.Stop();
-                    StopThresholdTimer();
-                }
-                // Consider threshold as percentage
-                if (CountThreshold >= 0 && CountThreshold < 1)
-                {
-                    //TODO: Check if _done pass thresold
-                }
-                else if (CountThreshold >= 1)
-                {
-                    //TODO: implement a raise mode to permit advance of values different of 1
-                    if (done % CountThreshold == 0) OnCountProgress(Done, Total);
-                }
-            }
-        }
-        public double? DoneProportion
-        {
-            get => Done / Total;
-            set => Done = (value * Total) ?? 0;
-        }
-        public double? ToDo
-        {
-            get => Total - Done;
-            set => Done = (Total - ToDo) ?? 0;
-        }
-        public double? ToDoProportion
-        {
-            get => 1 - DoneProportion;
-            set => DoneProportion = 1 - value;
-        }
+    public double? ItemPerSecond => Done > 0 && DoneTime > TimeSpan.Zero ? Done / DoneTime.TotalSeconds : null;
 
-        public double? CountThreshold { get; set; }
-        public TimeSpan? TimeThreshold { get; set; }
+    #region Management
+    public IEtaCalculator Start()
+    {
+        StartTime = DateTimeOffset.Now;
+        stopwatch.Start();
+        StartThresholdTimer();
+        return this;
+    }
+    public IEtaCalculator Advance(double progressAdd)
+    {
+        Done += progressAdd;
+        if (Total.HasValue) Done = Done.MaxCap(Total.Value);
+        return this;
+    }
+    public IEtaCalculator Complete()
+    {
+        Done = Total ?? double.MaxValue;
+        return this;
+    }
+    #endregion
 
-        public TimeSpan? TotalTime
-        {
-            get
-            {
-                if (!Total.HasValue || Done == 0) return null;
-                else return TimeSpan.FromMilliseconds(stopwatch.Elapsed.TotalMilliseconds / Done * Total.Value);
-            }
-        }
-        public TimeSpan DoneTime => stopwatch.Elapsed;
-        public TimeSpan? ToDoTime => TotalTime?.Subtract(DoneTime);
+    public override string ToString()
+    {
+        return $"Tot: {Total:n0} [{TotalTime:d\\.hh\\:mm\\:ss}] - " +
+            $"Done: {Done:n0} ({DoneProportion:p0}) [{DoneTime:d\\.hh\\:mm\\:ss}] - " +
+            $"ToDo: {ToDo:n0} ({ToDoProportion:p0}) [{ToDoTime:d\\.hh\\:mm\\:ss}] => " +
+            $"{ItemPerSecond:n} item/s, From {StartTime:d} {StartTime:t} To {StopTime:d} {StopTime:t}";
+    }
 
-        public double? ItemPerSecond => Done > 0 && DoneTime > TimeSpan.Zero ? Done / DoneTime.TotalSeconds : null;
-
-        #region Management
-        public IEtaCalculator Start()
-        {
-            StartTime = DateTimeOffset.Now;
-            stopwatch.Start();
-            StartThresholdTimer();
-            return this;
-        }
-        public IEtaCalculator Advance(double progressAdd)
-        {
-            Done += progressAdd;
-            if (Total.HasValue) Done = Done.MaxCap(Total.Value);
-            return this;
-        }
-        public IEtaCalculator Complete()
-        {
-            Done = Total ?? double.MaxValue;
-            return this;
-        }
-        #endregion
-
-        public override string ToString()
-        {
-            return $"Tot: {Total:n0} [{TotalTime:d\\.hh\\:mm\\:ss}] - " +
-                $"Done: {Done:n0} ({DoneProportion:p0}) [{DoneTime:d\\.hh\\:mm\\:ss}] - " +
-                $"ToDo: {ToDo:n0} ({ToDoProportion:p0}) [{ToDoTime:d\\.hh\\:mm\\:ss}] => " +
-                $"{ItemPerSecond:n} item/s, From {StartTime:d} {StartTime:t} To {StopTime:d} {StopTime:t}";
-        }
-
-        public IEtaCalculator SetTotal(double? total)
-        {
-            Total = total;
-            return this;
-        }
+    public IEtaCalculator SetTotal(double? total)
+    {
+        Total = total;
+        return this;
     }
 }
--- Zu1779.EtaCalculator/Zu1779.EtaCalculator/EtaExtension.cs
+++ Zu1779.EtaCalculator/Zu1779.EtaCalculator/EtaExtension.cs
@@ -6,7 +6,40 @@
 
     public static class EtaExtension
     {
-        public static IEnumerable<T> TrackProgress<T>(this IEnumerable<T> ien, IEtaCalculator calculator = null) =>
-            calculator == null ? ien : calculator.TrackProgress(ien);
+        public static IEnumerable<T> TrackProgress<T>(this IEnumerable<T> ien, IEtaCalculator calculator = null) 
+            => calculator == null ? ien : calculator.TrackProgress(ien);
+
+        public static IEnumerable<T> TrackForEachProgress<T>(this IEnumerable<T> ien, TimeSpan tsProgress, Action<EtaCalculator> progressAction)
+            => ien.TrackForEachProgress(tsProgress, progressAction, ien => ien.Count(), _ => 1);
+
+        public static IEnumerable<T> TrackForEachProgress<T>(this IEnumerable<T> ien, TimeSpan tsProgress, Action<EtaCalculator> progressAction, 
+            Func<IEnumerable<T>, double> total, Func<T, double> advance) {
+
+            EtaCalculator eta = new EtaCalculator();
+            eta.TimeThreshold = tsProgress;
+            eta.TimeProgress += (s, a) => progressAction((EtaCalculator)s);
+            eta.SetTotal(total(ien));
+            eta.Done = 0;
+            eta.Start();
+            foreach (var item in ien) {
+                yield return item;
+                eta.Advance(advance(item));
+            }
+            eta.Complete();
+        }
+
+        // public static IEnumerable<T> TrackForEachProgress<T>(this IEnumerable<T> ien, TimeSpan tsProgress, Action<EtaCalculator> progressAction) {
+        //     EtaCalculator eta = new EtaCalculator();
+        //     eta.TimeThreshold = tsProgress;
+        //     eta.TimeProgress += (s, a) => progressAction((EtaCalculator)s);
+        //     eta.SetTotal(ien.Count());
+        //     eta.Done = 0;
+        //     eta.Start();
+        //     foreach (var item in ien) {
+        //         yield return item;
+        //         eta.Advance(1);
+        //     }
+        //     eta.Complete();
+        // }
     }
 }
